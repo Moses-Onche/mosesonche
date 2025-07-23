8dcefbbb206c93113fd0e694818040d384502d25
Moses-Onche <onchemoses38@gmail.com>
2025-07-24 00:37:36 +0100

    Fix: Layout on page zoom

--- script.js
+++ script.js
@@ -3,13 +3,13 @@ let height = "";
 
 projectContainer.forEach(project => {
     project.addEventListener('click', () => {
-      if (project.children[1].children[3].children[0].children.length < 4) height = "230px";
-      else height = "250px";
+      if (project.children[1].children[3].children[0].children.length < 4) height = "160px";
+      else height = "170px";
 
       if (project.style.height == "") {
         project.style.height = height;
       } else if (project.style.height == height) {
-        project.style.height = "115px";
+        project.style.height = "80px";
       } else {
         project.style.height = height;
       }
