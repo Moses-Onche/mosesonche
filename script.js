996eea4d0d4f77a849db0a994b75d7d61ad6b53b
Moses-Onche <onchemoses38@gmail.com>
2025-07-23 23:24:26 +0100

    Feat: animate project div to respond to clicks

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 const projectContainer = document.querySelectorAll('.project');
-let open = false;
+let height = "";
 
 projectContainer.forEach(project => {
     project.addEventListener('click', () => {
-      if (!open) {
-        project.style.height = "240px";
-        open = true;
-      } else {
+      if (project.children[1].children[3].children[0].children.length < 4) height = "230px";
+      else height = "250px";
+
+      if (project.style.height == "") {
+        project.style.height = height;
+      } else if (project.style.height == height) {
         project.style.height = "115px";
-        open = false;
+      } else {
+        project.style.height = height;
       }
     });
 });
